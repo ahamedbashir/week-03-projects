f115037d3901b111a28146c96a9963f6984a86f7
Bashir Ahamed <ahamedb1991@gmail.com>
2019-09-16 01:10:10 -0400

    get cities by zipCode

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { async } from 'q';
 
 
 function City(props) {
-  return (<div></div>);
+  return (<div>
+    {props.getCities}
+  </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div></div>);
+  return (<div>
+    Zip Code: <input type="text" onChange = {(event) => props.getZipCode(event.target.value)} placeHolder = "Try 10016"></input>
+  </div>);
 }
 
 
 class App extends Component {
+  state = {
+    zipCode: "",
+    cities: "",
+    city: "",
+    zipCodes: ""
+  }
+
+  getZipCode = async (zip) => {
+    
+    console.log(zip);
+    if(zip.length === 5) {
+      this.setState({zipCode: zip});
+      const response = await fetch("http://ctp-zip-api.herokuapp.com/zip/" + zip);
+      const cities = await response.json();
+      console.log(cities);
+      this.setState({cities: cities})
+    }
+  }
+  getCities = async () => {
+    const response = await fetch("http://ctp-zip-api.herokuapp.com/zip/" + this.state.zipCode);
+    const cities = await response.json();
+    console.log(cities);
+    return cities;
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+        <ZipSearchField getZipCode = {this.getZipCode}/>
         <div>
-          <City />
+          <City  getCities = {this.getCities}/>
           <City />
         </div>
       </div>
