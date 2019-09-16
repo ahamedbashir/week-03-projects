bef471faa03df9b9cf59841142a7c63bad612d53
Bashir Ahamed <ahamedb1991@gmail.com>
2019-09-16 19:59:44 -0400

    complete project 1

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,33 @@
 import React, {Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {Card, Col} from 'react-bootstrap';
 
 function City(props) {
   const cities = props.cities;
   console.log(cities)
   return (
-  <div>
-    <h1>{cities.City}, {cities.State}</h1>
+  <div className = "mt-4">
+    <Card style={{width: '25rem'}}>
+    <Card.Header>{cities.City}, {cities.State}</Card.Header>
+    <Card.Body>
     <ul>
       <li>State: {cities.State}</li>
-      <li>Location: {cities.Lat}, {cities.Long}</li>
-      <li>Population: {cities.EstimatedPopulation}</li>
+      <li>Location: ({cities.Lat}, {cities.Long})</li>
+      <li>Population (estimated): {cities.EstimatedPopulation}</li>
       <li>Total Wages: {cities.TotalWages}</li>
     </ul>
+    </Card.Body>
+    </Card>  
   </div>);
 }
 
 function ZipSearchField(props) {
-  return (<div>
-    Zip Code: <input type="number" onChange = {(event) => props.getCities(event.target.value)} placeholder = "Try 10016"></input>
-  </div>);
+  return (
+  <div className = "body form-inline body row mt-3" style={{fontWeight: "bold"}}>
+    <div style={{width: '25rem'}}> Zip Code: <input type="number" className="form-control form-inline"  onChange = {(event) => props.getCities(event.target.value)} placeholder = "Try 10016"></input></div>
+  </div>
+  );
 }
 
 
@@ -56,14 +62,16 @@ class App extends Component {
         </div>
         <ZipSearchField getCities= {this.getCities}/>
         {this.state.cities?
-          <div>
+          <div className = "body">
             {this.state.cities.map(city => {
               return <City cities = {city}/>
               })}
           </div>
-          : <div>No Results</div>
+          : <div className = "body mt-3">
+            <div style={{width: '25rem'}}>No Results</div>
+            </div>
         }
-    </div>
+        </div>  
     );
   }
 }
